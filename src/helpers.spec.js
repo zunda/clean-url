2b319e4735a32e4b35e9f1731d8661537c4a79f3
zunda <zundan@gmail.com>
2026-02-13 14:28:21 -1000

    Document more about parsing URLs

--- src/helpers.spec.js
+++ src/helpers.spec.js
@@ -9,6 +9,36 @@ describe('parseUrl', () => {
       expect(parsed[prop]).toBe(goal)
     })
   })
+
+  test('parses a full URL', () => {
+    const parsed = parseUrl('https://user:pass@www.example.com/path/to?q=query&p=parameter#fragment')
+    const expected = {
+      hash: '#fragment',
+      host: 'www.example.com',
+      password: 'pass',
+      pathname: '/path/to',
+      protocol: 'https:',
+      search: '?q=query&p=parameter',
+      username: 'user'
+    }
+    Object.entries(expected).forEach(([prop, goal]) => {
+      expect(parsed[prop]).toBe(goal)
+    })
+  })
+
+  test('parses an http URL with non-standard port', () => {
+    const parsed = parseUrl('http://example.com:8080')
+    const expected = { host: 'example.com:8080' }
+    Object.entries(expected).forEach(([prop, goal]) => {
+      expect(parsed[prop]).toBe(goal)
+    })
+  })
+
+  test('parses an https URL with non-standard port', () => {
+    const parsed = parseUrl('https://example.com:80')
+    const expected = { host: 'example.com:80' }
+    Object.entries(expected).forEach(([prop, goal]) => {
+      expect(parsed[prop]).toBe(goal)
+    })
+  })
 })
-
-

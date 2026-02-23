6b8672d2cba21931daf619a97924f10b0cad8c38
zunda <zundan@gmail.com>
2026-02-23 11:32:24 -1000

    Test minimal feature for UrlCleaner component

---
+++ src/tests/UrlCleaner.spec.js
@@ -0,0 +1,18 @@
+import { render, fireEvent, screen } from '@testing-library/vue'
+import UrlCleanerComponent from '../components/UrlCleaner.vue'
+
+describe('UrlCleanerComponent', () => {
+  test('removes queries from a URL', async () => {
+    render(UrlCleanerComponent)
+    const dirty = screen.getByPlaceholderText('Dirty URL')
+    const clean = screen.getByPlaceholderText('Clean URL')
+
+    const dirtyUrl = 'http://example.com/path/?p=parameter&v=keep'
+    const cleanUrl = 'http://example.com/path/?v=keep'
+
+    await fireEvent.update(dirty, dirtyUrl)
+
+    expect(dirty.value).toBe(dirtyUrl)
+    expect(clean.value).toBe(cleanUrl)
+  })
+})
